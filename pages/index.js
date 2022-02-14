28187ba661138d121a10a51cd5e251062573a632
Gri-ffin <gameshunter359@gmail.com>
2022-02-14 12:52:29 +0100

    Add the section section to the index page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { Fragment } from 'react';
 import { SectionOne } from '../components/IndexPage/SectionOne';
 import Head from 'next/head';
+import { SectionTwo } from '../components/IndexPage/SectionTwo';
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <link rel='icon' href='/images/favicon.ico' />
       </Head>
       <SectionOne />
+      <SectionTwo />
     </Fragment>
   );
 }
