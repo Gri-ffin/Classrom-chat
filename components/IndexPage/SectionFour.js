4dbfcbb458372187e90975a6c0d6788ef069929c
Gri-ffin <gameshunter359@gmail.com>
2022-02-14 13:35:53 +0100

    Create Section four for index page

---
+++ components/IndexPage/SectionFour.js
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import fandomLandingSVG from '../../public/assets/fandom_landing.svg';
+
+export const SectionFour = () => {
+  return (
+    <section className='relative flex flex-row'>
+      <div>
+        <Image
+          src={fandomLandingSVG}
+          alt='Fandom landing svg'
+          height={1500}
+          width={1500}
+        />
+      </div>
+      <div className='mt-[280px] text-center mr-4'>
+        <h2 className='text-black font-bold text-4xl'>From few to a fandom</h2>
+        <p className='m-4 leading-loose'>
+          Get any community running with moderation tools and custom member
+          access. Give members special powers, set up private channels, and
+          more.
+        </p>
+      </div>
+    </section>
+  );
+};
