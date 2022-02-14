bd61c899366439ddc7deda91753578e96f84d581
Gri-ffin <gameshunter359@gmail.com>
2022-02-14 13:36:08 +0100

    Add section four to the index page

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { SectionOne } from '../components/IndexPage/SectionOne';
 import Head from 'next/head';
 import { SectionTwo } from '../components/IndexPage/SectionTwo';
 import { SectionThree } from '../components/IndexPage/SectionThree';
+import { SectionFour } from '../components/IndexPage/SectionFour';
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <SectionOne />
       <SectionTwo />
       <SectionThree />
+      <SectionFour />
     </Fragment>
   );
 }
