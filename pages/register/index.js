9dc5459ecb0dc3efe45fa3890ef61744ee635e3b
Gri-ffin <gameshunter359@gmail.com>
2022-02-16 14:43:04 +0100

    Create the signup page

---
+++ pages/register/index.js
@@ -0,0 +1,9 @@
+import { Signup } from '../../components/Signup/Signup';
+
+export default function Register() {
+  return (
+    <main className='bg-signupBg bg-center bg-no-repeat bg-cover h-[100vh]'>
+      <Signup />
+    </main>
+  );
+}
