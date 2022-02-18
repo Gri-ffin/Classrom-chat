cd3517128591fc20741f65e26580762bb650eaee
Gri-ffin <gameshunter359@gmail.com>
2022-02-18 13:52:38 +0100

    Add Head tag to the register page

--- pages/register/index.js
+++ pages/register/index.js
@@ -1,9 +1,18 @@
+import Head from 'next/head';
+import { Fragment } from 'react';
 import { Signup } from '../../components/Signup/Signup';
 
 export default function Register() {
   return (
-    <main className='bg-signupBg bg-center bg-no-repeat bg-cover h-[100vh]'>
-      <Signup />
-    </main>
+    <Fragment>
+      <Head>
+        <title>Discord</title>
+        <meta name='description' content='discord | Create account' />
+        <link rel='icon' href='/images/favicon.ico' />
+      </Head>
+      <main className='bg-signupBg bg-center bg-no-repeat bg-cover h-[100vh]'>
+        <Signup />
+      </main>
+    </Fragment>
   );
 }
