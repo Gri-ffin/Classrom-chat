d30b55d454cd655cc42ef93c93908d9e1a78c742
Gri-ffin <gameshunter359@gmail.com>
2022-02-22 17:49:33 +0100

    Create sidebar icons

---
+++ components/Dashboard/SideBarIcon.js
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+
+export const SidebarIcon = ({ icon, text }) => {
+  return (
+    <div className='sidebar-icon group'>
+      <Image src={icon} alt='icon' width={40} height={40} />
+      <span className='sidebar-tooltip group-hover:scale-100'>{text}</span>
+    </div>
+  );
+};
