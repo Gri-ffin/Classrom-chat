8313f7e6a7121c68bc2e21a438f797db31deab8e
Gri-ffin <gameshunter359@gmail.com>
2022-02-22 19:34:14 +0100

    create chat route

--- pages/chat/index.js
+++ pages/chat/index.js
@@ -1,9 +1,20 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useAuth } from '../../components/context/AuthContext';
 import { SideBar } from '../../components/Dashboard/Sidebar';
+import { Chatroom } from '../../components/Dashboard/Chatroom';
 
 export default function Chat() {
+  const { currentUser } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    !currentUser && router.push('/login');
+  }, [currentUser, router]);
   return (
     <div className='flex'>
       <SideBar />
+      <Chatroom />
     </div>
   );
 }
