ae3ae4ecb2edd87a693b9b64ac39b5bd4fc312ac
Gri-ffin <gameshunter359@gmail.com>
2022-02-22 17:49:45 +0100

    Create sidebar and the styles

---
+++ components/Dashboard/Sidebar.js
@@ -0,0 +1,18 @@
+import { SidebarIcon } from './SideBarIcon';
+import discordLogo from '../../public/assets/discord_mini_icon.svg';
+import nitroIcon from '../../public/assets/nitro_icon.svg';
+import plusIcon from '../../public/assets/plus_icon.svg';
+import dmIcon from '../../public/assets/dm_icon.svg';
+import icon from '../../public/assets/close_icon.svg';
+
+export const SideBar = () => {
+  return (
+    <div className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary shadow-lg'>
+      <SidebarIcon icon={discordLogo} text='Discord' />
+      <SidebarIcon icon={nitroIcon} text='Nitro' />
+      <SidebarIcon icon={plusIcon} text='Add Friend' />
+      <SidebarIcon icon={dmIcon} text='Direct message' />
+      <SidebarIcon icon={icon} text='Logout' />
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
+  mode: 'jit',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
@@ -9,6 +12,21 @@ module.exports = {
         clouds: "url('/assets/center_bg_landing_header.svg')",
         signupBg: "url('/assets/login_bg.svg')",
         loginBg: "url('/assets/login_bg.png')",
+      },
+      colors: {
+        primary: '#202225',
+        secondary: '#5865f2',
+        gray: colors.neutral,
+        gray: {
+          900: '#202225',
+          800: '#2f3136',
+          700: '#36393f',
+          600: '#4f545c',
+          400: '#d4d7dc',
+          300: '#e3e5e8',
+          200: '#ebedef',
+          100: '#f2f3f5',
+        },
       },
     },
   },
