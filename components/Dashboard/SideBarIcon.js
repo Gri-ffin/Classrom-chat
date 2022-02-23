f52a399590156ce258191233f5251e9f9cc90ed3
Gri-ffin <gameshunter359@gmail.com>
2022-02-23 19:56:04 +0100

    Redirect to main page when user click discord icon

--- components/Dashboard/SideBarIcon.js
+++ components/Dashboard/SideBarIcon.js
@@ -1,8 +1,13 @@
 import Image from 'next/image';
 
-export const SidebarIcon = ({ icon, text }) => {
+export const SidebarIcon = ({ icon, text, clickHandler }) => {
   return (
-    <div className='sidebar-icon group'>
+    <div
+      className='sidebar-icon group'
+      onClick={() => {
+        clickHandler();
+      }}
+    >
       <Image src={icon} alt='icon' width={40} height={40} />
       <span className='sidebar-tooltip group-hover:scale-100'>{text}</span>
     </div>
--- components/Dashboard/Sidebar.js
+++ components/Dashboard/Sidebar.js
@@ -4,11 +4,21 @@ import nitroIcon from '../../public/assets/nitro_icon.svg';
 import plusIcon from '../../public/assets/plus_icon.svg';
 import dmIcon from '../../public/assets/dm_icon.svg';
 import icon from '../../public/assets/close_icon.svg';
+import { useRouter } from 'next/router';
 
 export const SideBar = () => {
+  const router = useRouter();
+  function discordIconClickHandler() {
+    router.push('/');
+  }
+
   return (
     <div className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary shadow-lg'>
-      <SidebarIcon icon={discordLogo} text='Discord' />
+      <SidebarIcon
+        icon={discordLogo}
+        text='Discord'
+        clickHandler={discordIconClickHandler}
+      />
       <SidebarIcon icon={nitroIcon} text='Nitro' />
       <SidebarIcon icon={plusIcon} text='Add Friend' />
       <SidebarIcon icon={dmIcon} text='Direct message' />
