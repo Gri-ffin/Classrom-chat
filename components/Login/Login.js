f6b99c39fef00321c13cddf73e88704776f47c69
Gri-ffin <gameshunter359@gmail.com>
2022-02-25 23:01:01 +0100

    Remove unecessary code

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -1,121 +1,41 @@
-import { getAuth } from 'firebase/auth';
-import Image from 'next/image';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { useAuth } from '../context/AuthContext';
-import app from '../Helpers/firebase';
+import { loginHelper } from '../Helpers/functions';
+import { GoogleIcon } from './GoogleIcon';
 
 export const Login = () => {
-  const emailRef = useRef();
-  const passwordRef = useRef();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
-  const { login, loginWithGoogle } = useAuth();
+  const { loginWithGoogle } = useAuth();
   const router = useRouter();
 
-  async function formSubmitHandler(e) {
-    e.preventDefault();
-    try {
-      setLoading(true);
-      setError('');
-      await login(emailRef.current.value, passwordRef.current.value);
-      router.push('/chat');
-    } catch (e) {
-      setError(e.code.split('/')[1]);
-    }
-    setLoading(false);
-    emailRef.current.value = '';
-    passwordRef.current.value = '';
-  }
   async function googleLoginHandler(e) {
     e.preventDefault();
-    try {
-      setLoading(true);
-      setError('');
-      await loginWithGoogle();
-      router.push('/chat');
-    } catch (e) {
-      setError(e.code.split('/')[1]);
-    }
-    setLoading(false);
+    await loginHelper(setLoading, setError, loginWithGoogle, router);
   }
 
   return (
-    <div className='text-center w-[60%] m-auto absolute top-[150px] left-[20%] bg-gray-700 text-white rounded-md p-[16px] shadow-2xl'>
-      <div className='relative flex flex-row text-center p-4'>
-        <div className='px-5 w-[300%]'>
-          <h2 className='font-bold text-2xl pr-24'>Hey, You&apos;re back !</h2>
-          <p className='text-gray-400 pt-2 pr-24'>
-            We are very happy to see you again !
-          </p>
-          <div className='bg-red-400 text-red-700 w-[83%]'>{error}</div>
-          <form onSubmit={formSubmitHandler}>
-            <label htmlFor='email' className='block text-gray-300 text-left'>
-              Email
-            </label>
-            <input
-              type='email'
-              id='email'
-              className='block my-3 bg-gray-800 py-1 w-[83%] rounded-sm'
-              ref={emailRef}
-            />
-            <label htmlFor='password' className='block text-gray-300 text-left'>
-              Password
-            </label>
-            <input
-              type='password'
-              id='password'
-              className='block mt-3 bg-gray-800 py-1 w-[83%] rounded-sm'
-              ref={passwordRef}
-            />
-            <p className='text-blue-500 text-left text-sm hover:underline hover:cursor-pointer'>
-              Forgot your own password?
-            </p>
-            <p className='text-gray-400 text-sm inline text-left mr-[80%]'>
-              Or Login with
-              <span>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='16'
-                  height='16'
-                  fill='red'
-                  className='bi bi-google hover:cursor-pointer inline-block ml-1'
-                  viewBox='0 0 16 16'
-                  onClick={googleLoginHandler}
-                >
-                  <path d='M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z' />
-                </svg>
-              </span>
-            </p>
-            <button
-              disabled={loading}
-              className='block p-2 mt-4 bg-blue-600 w-[83%] hover:bg-blue-800 mb-2 rounded-smdisabled:opacity-75 disabled:cursor-not-allowed'
-            >
-              {!loading ? 'Connect' : 'Loading...'}
-            </button>
-            <p className='text-left text-sm text-gray-500'>
-              Need an account?
-              <Link href='/register'>
-                <a className='text-blue-500 hover:underline'>Register</a>
-              </Link>
-            </p>
-          </form>
+    <div className='text-center w-[60%] m-auto absolute top-[30%] left-[20%] bg-gray-700 text-white rounded-md p-[16px] shadow-2xl'>
+      {error && <div className='bg-red-500 text-white text-xl'>{error}</div>}
+      <div className='relative flex flex-col text-center p-4'>
+        <h2 className='text-4xl font-bold text-gray-500 underline mb-5'>
+          Welcome traveller, To a new world
+        </h2>
+        <div className='bg-gray-800 h-16'>
+          <button
+            className='text-gray-400 text-3xl p-3 hover:underline'
+            onClick={googleLoginHandler}
+          >
+            <GoogleIcon />
+            Login with Google
+          </button>
         </div>
-        <div className='pr-12'>
-          <Image
-            src='/assets/qr_code.png'
-            alt='Qr code'
-            width={700}
-            height={700}
-          />
-          <h2 className='font-bold text-2xl'>Connect with a qr code</h2>
-          <p className='text-gray-400'>
-            Scan it with
-            <span className='text-gray-300'> the mobile application</span> to
-            instantly connect.
-          </p>
-        </div>
+        {loading && (
+          <div className=' flex justify-center items-center'>
+            <div className='animate-spin rounded-full h-12 w-12 mt-2 border-b-2 border-white'></div>
+          </div>
+        )}
       </div>
     </div>
   );
