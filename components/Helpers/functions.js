4f611ee44a9bbec0bb95299f72b6178de7c7fd0c
Gri-ffin <gameshunter359@gmail.com>
2022-02-25 23:00:25 +0100

    Add helper function for authentication

--- components/Helpers/functions.js
+++ components/Helpers/functions.js
@@ -5,3 +5,16 @@ export const validateEmail = (email) => {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
 };
+
+// Help with user authentication
+export const loginHelper = async (setLoading, setError, loginFunc, router) => {
+  try {
+    setLoading(true);
+    setError('');
+    await loginFunc();
+    router.push('/chat');
+  } catch (e) {
+    setError(e.code.split('/')[1]);
+  }
+  setLoading(false);
+};
