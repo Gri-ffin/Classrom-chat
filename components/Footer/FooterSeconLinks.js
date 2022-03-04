14bc144beb7b940d10ec042659f6e8e520fa59ca
~Blank~ <82527700+Gri-ffin@users.noreply.github.com>
2022-03-04 20:03:27 +0100

    Update FooterSeconLinks.js

---
+++ components/Footer/FooterSeconLinks.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Link from 'next/link';
+
+export function FooterSecondLinks() {
+  return (
+    <ul>
+      <li className='py-2'>
+        <Link href='/enterprise'>
+          <a className='text-blue-700'>Enterprise</a>
+        </Link>
+      </li>
+      <li className='py-2'>
+        <Link href='/about'>
+          <a className='text-white'>About</a>
+        </Link>
+      </li>
+      <li className='py-2'>
+        <Link href='/jobs'>
+          <a className='text-white'>Jobs</a>
+        </Link>
+      </li>
+      <li className='py-2'>
+        <Link href='/graphic-charter'>
+          <a className='text-white'>Graphic charter</a>
+        </Link>
+      </li>
+      <li className='py-2'>
+        <Link href='/news'>
+          <a className='text-white'>News Space</a>
+        </Link>
+      </li>
+    </ul>
