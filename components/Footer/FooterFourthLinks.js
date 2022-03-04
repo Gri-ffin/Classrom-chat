9009566fbc2fe5a54e8ab4d4fb0159114c98874c
~Blank~ <82527700+Gri-ffin@users.noreply.github.com>
2022-03-04 20:05:27 +0100

    Update FooterFourthLinks.js

--- components/Footer/FooterFourthLinks.js
+++ components/Footer/FooterFourthLinks.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 
-export function FooterThirdLinks({}) {
+export function FooterFourthLinks({}) {
   return (
     <ul>
       <li className='py-2'>
